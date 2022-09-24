a1f7a3624b59ab5727183a465e3c6d8bf4092789
Sergey <serg.garifullin@mail.ru>
2022-09-24 20:20:52 +0300

    first task

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const film = prompt('Один из промотренных фильмов?', ''),
+	ocenka = +prompt('На сколько оцените его?', ''),
+	films = prompt('Один из промотренных фильмов?', ''),
+	ocenochka = +prompt('На сколько оцените его?', '');
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+personalMovieDB.movies[film] = ocenka;
+personalMovieDB.movies[films] = ocenochka;
 
 
+console.log(personalMovieDB);
 
 
 
