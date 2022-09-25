bc27ae147da9876edd26ecbf5d8915a5732a760e
Sergey <serg.garifullin@mail.ru>
2022-09-25 19:04:11 +0300

    Cucle function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
 
-const film = prompt('Один из промотренных фильмов?', ''),
-	ocenka = +prompt('На сколько оцените его?', ''),
-	films = prompt('Один из промотренных фильмов?', ''),
-	ocenochka = +prompt('На сколько оцените его?', '');
+function start() {
+	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+	}
+}
+start();
+
 
 const personalMovieDB = {
 	count: numberOfFilms,
@@ -13,12 +18,49 @@ const personalMovieDB = {
 	privat: false
 };
 
-personalMovieDB.movies[film] = ocenka;
-personalMovieDB.movies[films] = ocenochka;
+function rememberMyFilms() {
+	for (let i = 0; i < 2; i++) {
+		const film = prompt('Один из промотренных фильмов?', ''),
+			ocenka = +prompt('На сколько оцените его?', '');
+
+		if (film != null && ocenka != null && film != '' && ocenka != '' && film.length < 50) {
+			personalMovieDB.movies[film] = ocenka;
+		} else {
+			console.log('error');
+			i--;
+		}
+	}
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+	if (personalMovieDB.count >= 1 && personalMovieDB.count <= 10) {
+		alert('Просмотренно довольно мало фильмов');
+	} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+		alert('Вы классический зритель');
+	} else if (personalMovieDB.count > 30) {
+		alert('Вы киноман!');
+	} else {
+		alert('Произошла ошибка');
+	}
+}
+
+detectPersonalLevel();
 
 
-console.log(personalMovieDB);
+function showMyDB(hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB);
+	}
+}
 
+showMyDB(personalMovieDB.privat);
 
+function writeYourGenres() {
+	for (let i = 1; i < 4; i++) {
+		personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+	}
+}
 
-
+writeYourGenres();
